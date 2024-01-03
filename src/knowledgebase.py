ac0b7694f37784e47f47ef739b8719ddb865dd93
Le Viet <leviettt.tnh@gmail.com>
2024-01-04 03:47:14 +0700

    upd: Fix minor bug

--- src/knowledgebase.py
+++ src/knowledgebase.py
@@ -1,35 +1,14 @@
 class KnowledgeBase:
-    def __init__(self, world_size):
-        self.percept_reason = { 'B': 'P', 'S': 'W' }
-        self.world_size = world_size
-        self.knowledge_base = [[{'P': False, 'W': False, 'B': False, 'S': False, 'Safe': False} for _ in range(world_size)] for _ in range(world_size)]
+    def __init__(self):
+        self.knowledge_base = set()
 
-    def update(self, position, percept, status):
-        x, y = position
-        print('Update', position, percept, status)
-        new_dict = dict(self.knowledge_base[x][y])
-        new_dict[percept] = status
-        self.knowledge_base[x][y] = new_dict
-        if percept == 'Safe' and status:
-            self.update(position, 'P', False)
-            self.update(position, 'W', False)
+    def update(self, percept, value):
+        if value:
+            if percept not in self.knowledge_base:
+                self.knowledge_base.add(percept)
+        else:
+            if percept in self.knowledge_base:
+                self.knowledge_base.remove(percept)
 
-    def get_percept_safe(self, position):
-        x, y = position
-        return self.knowledge_base[x][y]['Safe']
-    
-    def get_percept_pit(self, position):
-        x, y = position
-        return self.knowledge_base[x][y]['P']
-    
-    def get_percept_wumpus(self, position):
-        x, y = position
-        return self.knowledge_base[x][y]['W']
-    
-    def get_percept_breeze(self, position):
-        x, y = position
-        return self.knowledge_base[x][y]['B']
-    
-    def get_percept_stench(self, position):
-        x, y = position
-        return self.knowledge_base[x][y]['S']
+    def check(self, percept):
+        return percept in self.knowledge_base
